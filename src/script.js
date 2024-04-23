6e2a97fe0971e0c05ea94f72e498a46f4bde387e
Pâmella Namie Nomura <90418544+Baabie@users.noreply.github.com>
2024-04-23 13:41:48 -0300

    correcao

--- src/script.js
+++ src/script.js
@@ -7,35 +7,34 @@ app.use(cors());
 
 app.use(express.json());
 
-let veiculos = [];
+let vehicles = [];
 
 // Função para gerar um ID único
 function generateID() {
-    return veiculos.length > 0 ? veiculos[veiculos.length - 1].id + 1 : 1;
+  return vehicles.length > 0 ? vehicles[vehicles.length - 1].id + 1 : 1;
 }
 
-// Rota para criar um veículo
-app.post("/veiculos", (req, res) => {
-    const { modelo, marca, ano, cor, preco } = req.body;
-    const id = generateID();
-    const veiculo = { id, modelo, marca, ano, cor, preco };
-    veiculos.push(veiculo);
-    res.status(201).json(veiculo);
+// 1 - Endpoint para criar veículo
+app.post("/vehicles", (req, res) => {
+  const { modelo, marca, ano, cor, preco } = req.body;
+  const id = generateID();
+  const vehicle = { id, modelo, marca, ano, cor, preco };
+  vehicles.push(vehicle);
+  res.status(201).json(vehicle);
 });
 
-// Rota para ler todos os veículos
-app.get("/veiculos", (req, res) => {
-    res.json(veiculos);
+// 2 - Endpoint para ler todos os veículos
+app.get("/vehicles", (req, res) => {
+  res.json(vehicles);
 });
 
-// Rota para filtrar veículos por marca
-app.get("/veiculos/filtrar", (req, res) => {
-    const { marca } = req.query;
-    const veiculosFiltrados = veiculos.filter(
-        (veiculo) => veiculo.marca.toLowerCase() === marca.toLowerCase()
-    );
-    res.json(veiculosFiltrados);
+// 3 - Endpoint para filtrar veículos por marca
+app.get("/vehicles/filtrar", (req, res) => {
+  const { marca } = req.query;
+  const filteredVehicles = vehicles.filter(
+    (vehicle) => vehicle.marca.toLowerCase() === marca.toLowerCase()
+  );
+  res.json(filteredVehicles);
 });
-
 
 app.listen(3333, () => console.log("Servidor rodando na porta 3333"));
